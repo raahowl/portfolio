92d3d94c914d85dfdc77915180c7cdedd696dcfc
Rahul Suthar <rahulsuthar2030@gmail.com>
2024-05-20 22:38:56 +0530

    Updates

--- src/App.js
+++
@@ -1,9 +0,0 @@
-import 'bootstrap/dist/css/bootstrap.min.css'
-
-import CustomRoutes from './navigation'
-import React from 'react'
-
-const App = () => <CustomRoutes />
-// const App = () => <h1>Pranjal </h1>
-
-export default App
